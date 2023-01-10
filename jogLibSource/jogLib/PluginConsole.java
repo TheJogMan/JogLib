37eec5eb689d28aaa6cb5e08947e67d785889389
TheJogMan <jogoyo620@gmail.com>
2023-01-10 15:31:42 -0500

    Fixed vanilla command argument recognition and a hardcoded lookup table for vanilla command usages

--- jogLibSource/jogLib/PluginConsole.java
+++ jogLibSource/jogLib/PluginConsole.java
@@ -12,6 +12,7 @@ import jogUtil.indexable.*;
 import jogUtil.richText.*;
 import org.bukkit.*;
 import org.bukkit.command.*;
+import org.bukkit.command.defaults.*;
 import org.bukkit.event.*;
 import org.bukkit.event.player.*;
 import org.bukkit.event.server.*;
@@ -30,7 +31,7 @@ public class PluginConsole extends Console
 	}
 	
 	
-	private static SimpleCommandMap getCommandMap()
+	static SimpleCommandMap getCommandMap()
 	{
 		try
 		{
@@ -105,27 +106,26 @@ public class PluginConsole extends Console
 	}
 	
 	private record VanillaCommandFiller(PluginConsole console) implements ContextFiller
+	{
+		@Override
+		public Collection<CommandComponent> getComponents(Executor executor)
 		{
+			SimpleCommandMap commandMap = getCommandMap();
+			if (commandMap == null)
+				return null;
 			
-			@Override
-			public Collection<CommandComponent> getComponents(Executor executor)
+			ArrayList<CommandComponent> components = new ArrayList<>();
+			HashMap<Plugin, Category> plugins = new HashMap<>();
+			for (org.bukkit.command.Command command : commandMap.getCommands())
 			{
-				SimpleCommandMap commandMap = getCommandMap();
-				if (commandMap == null)
-					return null;
+				if (command instanceof ComponentHolder && console.equals(((ComponentHolder) command).component.parent()))
+					continue;
 				
-				ArrayList<CommandComponent> components = new ArrayList<>();
-				HashMap<Plugin, Category> plugins = new HashMap<>();
-				for (org.bukkit.command.Command command : commandMap.getCommands())
-				{
-					if (command instanceof ComponentHolder && console.equals(((ComponentHolder) command).component.parent()))
-						continue;
-					
-					components.add(new VanillaCommand(command));
-				}
-				return components;
+				components.add(new VanillaCommand(command));
 			}
+			return components;
 		}
+	}
 	
 	private static class VanillaCommand extends Command
 	{
@@ -133,88 +133,59 @@ public class PluginConsole extends Console
 		
 		public VanillaCommand(org.bukkit.command.Command command)
 		{
-			super(command.getName(), getDescriptionFromUsage(command.getDescription(), command.getUsage()));
+			super(command.getName(), command.getDescription());
 			command.getAliases().forEach(this::addAlias);
 			this.command = command;
-			if (command instanceof PluginCommand)
+			String[] usage = null;
+			if (command instanceof BukkitCommand && command.getDescription().equals("A Mojang provided command."))
 			{
-				String usage = parseUsage(command.getUsage())[0];
-				if (usage.length() > 0)
-				{
-					addArgument(VanillaArgumentsArgument.class, usage, new Object[] {false});
-					if (usage.charAt(0) != '<')
-						addArgumentList();
-				}
+				if (vanillaUsages.containsKey(command.getName()))
+					usage = vanillaUsages().get(command.getName());
+				else
+					Bukkit.getLogger().warning(command.getName() + " is a vanilla command with no set usage info, using default.");
 			}
 			else
 			{
+				String usageString = command.getUsage();
+				int index = usageString.indexOf(' ');
+				if (index != -1)
+					usage = new String[] {usageString.substring(index + 1)};
+				else
+					usage = new String[] {""};
+			}
+			
+			if (usage == null)
 				addArgument(VanillaArgumentsArgument.class);
-				addArgumentList();
+			else
+			{
+				boolean addEmpty = false;
+				boolean argumentsAdded = false;
+				for (String variant : usage)
+				{
+					if (variant.length() > 0)
+					{
+						argumentsAdded = true;
+						addArgument(VanillaArgumentsArgument.class, variant, new Object[]{false});
+						if (variant.charAt(0) == '[')
+							addEmpty = true;
+					}
+					else
+						addEmpty = true;
+				}
+				if (addEmpty && argumentsAdded)
+					addArgumentList();
 			}
 			addFilter(new PermissionFilter(command.getPermission(), command.getPermissionMessage()));
-		}
-		
-		static String getDescriptionFromUsage(String baseDescription, String usage)
-		{
-			String furtherDescription = parseUsage(usage)[1];
-			return baseDescription + (furtherDescription.length() > 0 ? '\n' + furtherDescription : "");
-		}
-		
-		static String[] parseUsage(String usage)
-		{
-			StringBuilder arguments = new StringBuilder();
-			StringBuilder furtherDescription = new StringBuilder();
-			int position = usage.indexOf(' ');
-			
-			if (position != -1)
-			{
-				position++;
-				boolean pastArguments = false;
-				int bracketDepth = 0;
-				while (position < usage.length())
-				{
-					char ch = usage.charAt(position);
-					if (pastArguments)
-						furtherDescription.append(ch);
-					else if (ch == '<' || ch == '[')
-					{
-						if (bracketDepth == 0 && arguments.length() > 0)
-							arguments.append(' ');
-						bracketDepth++;
-						arguments.append(ch);
-					}
-					else if (ch == '>' || ch == ']' && bracketDepth > 0)
-					{
-						bracketDepth--;
-						arguments.append(ch);
-					}
-					else if (bracketDepth > 0)
-						arguments.append(ch);
-					else if (bracketDepth == 0 && ch != ' ')
-					{
-						pastArguments = true;
-						furtherDescription.append(ch);
-					}
-					position++;
-				}
-			}
-			
-			return new String[] {arguments.toString(), furtherDescription.toString()};
 		}
 		
 		@Override
 		protected void execute(AdaptiveInterpretation result, Executor executor)
 		{
 			String[] arguments;
-			if (result.listNumber() == 1)
+			if (result.value().length > 0)
 			{
-				if (result.value().length > 0)
-				{
-					String string = (String) result.value()[0];
-					arguments = string.split(" ");
-				}
-				else
-					arguments = new String[0];
+				String string = (String) result.value()[0];
+				arguments = string.split(" ");
 			}
 			else
 				arguments = new String[0];
@@ -287,5 +258,92 @@ public class PluginConsole extends Console
 		{
 			return new ReturnResult<>(true, StringValue.consumeString(source));
 		}
+	}
+	
+	private static final HashMap<String, String[]> vanillaUsages = vanillaUsages();
+	
+	private static HashMap<String, String[]> vanillaUsages()
+	{
+		HashMap<String, String[]> usages = new HashMap<>();
+		
+		usages.put("give", new String[] {"<targets> <item> [<count>]"});
+		usages.put("summon", new String[] {"<entity> [<pos>]"});
+		usages.put("defaultgamemode", new String[] {"<gamemode>"});
+		usages.put("spreadplayers", new String[] {"<center> <spreadDistance> <maxRange> (<respectTeams>|under)"});
+		usages.put("jfr", new String[] {"<start|stop>"});
+		usages.put("tellraw", new String[] {"<targets> <message>"});
+		usages.put("bossbar", new String[] {"add <id> <name>", "remove <id>", "list", "set <id> (name|color|style|value|max|visible|players", "get <id> (value|max|visible|players)"});
+		usages.put("schedule", new String[] {"function <function> <time> [append|replace]", "clear <function>"});
+		usages.put("weather", new String[] {"clear [<duration>]", "rain [<duration>]", "thunder [<duration>]"});
+		usages.put("effect", new String[] {"clear [<targets>]", "give <targets> <effect> [<seconds>]"});
+		usages.put("stopsound", new String[] {"<targets> [*|master|music|record|weather|block|hostile|neutral|player|ambient|voice]"});
+		usages.put("place", new String[] {"feature <feature> [<pos>]", "jigsaw <pool> <target> <max_depth> [<position>]", "structure <structure> [<pos>]", "template <template> [<pos>]"});
+		usages.put("clear", new String[] {"[<targets>] [<item>]"});
+		usages.put("locate", new String[] {"structure <structure>", "biome <biome>", "poi <poi>"});
+		usages.put("teleport", new String[] {"<location>", "<destination>", "<targets> (<location>|<destination>)"});
+		usages.put("debug", new String[] {"start", "stop", "function <name>"});
+		usages.put("recipe", new String[] {"give <targets> (<recipe>|*)", "take <targets> (<recipe>|*)"});
+		usages.put("title", new String[] {"<targets> (clear|reset|title|subtitle|actionbar|times)"});
+		usages.put("fill", new String[] {"<from> <to> <block> [replace|keep|outline|hollow|destroy]"});
+		usages.put("clone", new String[] {"<begin> <end> <destination> [replace|masked|filtered]"});
+		usages.put("worldborder", new String[] {"add <distance> [<time>]", "set <distance> [<time>]", "center <pos>", "damage (amount|buffer)", "get", "warning (distance|time)"});
+		usages.put("teammsg", new String[] {"<message>"});
+		usages.put("say", new String[] {"<message>"});
+		usages.put("gamerule", new String[] {"<gamerule> [<value>]"});
+		usages.put("setblock", new String[] {"<pos> <block> [destroy|keep|replace]"});
+		usages.put("kill", new String[] {"[<targets>]"});
+		usages.put("xp", new String[] {"\"(add|set) <targets> <amount> [points|levels]\", \"query <targets> (points|levels)\""});
+		usages.put("time", new String[] {"set (day|noon|night|midnight|<time>)", "add <time>", "query (daytime|gametime|day)"});
+		usages.put("datapack", new String[] {"enable <name> [after|before|last|first]", "disable <name>", "list [available|enabled]"});
+		usages.put("setworldspawn", new String[] {"[<pos>] [<angle>]"});
+		usages.put("difficulty", new String[] {"[(peaceful|easy|normal|hard)]"});
+		usages.put("fillbiome", new String[] {"<from> <to> <biome> [replace]"});
+		usages.put("spawnpoint", new String[] {"[<targets>] [<pos>]"});
+		usages.put("banlist", new String[] {"[ips]", "[players]"});
+		usages.put("pardon", new String[] {"<targets>"});
+		usages.put("pardon-ip", new String[] {"<target>"});
+		usages.put("stop", new String[] {""});
+		usages.put("save-all", new String[] {"[flush]"});
+		usages.put("perf", new String[] {"start", "stop"});
+		usages.put("setidletimeout", new String[] {"<minutes>"});
+		usages.put("op", new String[] {"<targets>"});
+		usages.put("list", new String[] {"[uuids]"});
+		usages.put("data", new String[] {"(merge|get|remove|modify) (entity|block|storage)"});
+		usages.put("ban", new String[] {"<targets> [<reason>]"});
+		usages.put("function", new String[] {"<name>"});
+		usages.put("execute", new String[] {"{(if|unless) (block|biome|score|blocks|entity|predicate|data)|(as|at) <targets>|store (result|success)|positioned (<pos>|as)" +
+											"|rotated (<rot>|as)|facing (entity|<pos>)|align <axes>|anchored <anchor>|in <dimension>} run <command>"});
+		usages.put("playsound", new String[] {"<sound> (master|music|record|weather|block|hostile|neutral|player|ambient|voice)"});
+		usages.put("ban-ip", new String[] {"<target> [<reason>]"});
+		usages.put("forceload", new String[] {"add <from> [<to>]", "remove (<from>|all)", "query [<pos>]"});
+		usages.put("tp", new String[] {"\"<location>\", \"<destination>\", \"<targets> (<location>|<destination>)\""});
+		usages.put("tag", new String[] {"<targets> (add|remove|list)"});
+		usages.put("loot", new String[] {"replace (entity|block)", "(insert|spawn) <targetPos> (fish|loot|kill|mine)", "give <players> (fish|loot|kill|mine)"});
+		usages.put("me", new String[] {"action"});
+		usages.put("tm", new String[] {"<message>"});
+		usages.put("trigger", new String[] {"<objective> [add|set]"});
+		usages.put("save-on", new String[] {""});
+		usages.put("save-off", new String[] {""});
+		usages.put("experience", new String[] {"(add|set) <targets> <amount> [points|levels]", "query <targets> (points|levels)"});
+		usages.put("particle", new String[] {"<name> [<pos>]"});
+		usages.put("tell", new String[] {"<targets> <message>"});
+		usages.put("msg", new String[] {"<targets> <message>"});
+		usages.put("team", new String[] {"list [<team>]", "add <team> [<displayName>]", "(remove|empty|leave) <team>", "join <team> [<members>]", "modify <team> <setting>"});
+		usages.put("kick", new String[] {"targets [<reason>]"});
+		usages.put("deop", new String[] {"<targets>"});
+		usages.put("attribute", new String[] {"<target> <attribute> (get|base|modifier)"});
+		usages.put("gamemode", new String[] {"<gamemode> [<target>]"});
+		usages.put("scoreboard", new String[] {"objectives (list|add|modify|remove|setdisplay)", "players (list|set|get|add|remove|reset|enable|operation)"});
+		usages.put("seed", new String[] {""});
+		usages.put("whitelist", new String[] {"(on|off|list|reload)", "(add|remove) <targets>"});
+		usages.put("item", new String[] {"(replace|modify) (block|entity)"});
+		usages.put("reload", new String[] {""});
+		usages.put("advancement", new String[] {"(grant|revoke) <targets> (only|from|until|through|everything)"});
+		usages.put("spectate", new String[] {"[<target>] [<player>]"});
+		usages.put("enchant", new String[] {"<targets> <enchantment> [<level>]"});
+		usages.put("w", new String[] {"clear [<duration>]", "rain [<duration>]", "thunder [<duration>]"});
+		usages.put("help", new String[] {"[<command>]"});
+		
+		return usages;
 	}
 }
